13cf37ec9e56afc17aeb030e91f928b3a6747d32
Amauri DallOglio <amauridalloglio@outlook.com.br>
2023-04-04 11:18:20 -0300

    Pottencial 3 / 3 Três divisores

---
+++ Tres-Divisores.cs
@@ -0,0 +1,39 @@
+﻿namespace Desafio
+{
+    internal class Solucao
+    {
+        public void DesafioTresDivisores()
+        {
+            /*
+                Desafios de Código Básico C# - Pottencial
+              
+                Desafio
+                Dado um inteiro n, retorne true se n tiver exatamente três divisores positivos. Caso contrário, retorne false. O inteiro m é um divisor de n, se existe um inteiro k tal que n = k * m. 
+
+                Entrada
+                O arquivo de entrada consiste em um inteiro n que será validada caso esteja sob as condições do desafio
+         
+             */
+
+
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                // TODO: Crie as outras condições necessárias para a resolução do desafio:
+                if (n % i == 0)
+                {
+                    count++;
+                }
+                if (count > i)
+                {
+                    Console.WriteLine(false);
+                }
+            }
+            Console.WriteLine(count == 3);
+
+
+        }
+    }
+}
